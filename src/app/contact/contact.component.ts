ba5cf08423756a8cf7d33d76da07baba56c8da63
yunusyerli1 <yunus.yerli@gaboras.com>
2021-08-12 21:59:50 +0300

    contact form and validation has been added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { UtilsService } from '../services/utils.service';
+import { User } from '../store/user/user.model';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  contactForm: FormGroup;
+  user:any;
 
   countryList :Array<any> = [
     { id: "TR", name: "Turkey" },
@@ -18,9 +25,41 @@ export class ContactComponent implements OnInit {
     { id: "ZW", name: "Zimbabwe" }
   ]
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private utilsService: UtilsService,
+    private store:Store<{user:User}>
+    ) { }
 
   ngOnInit(): void {
+    this.createContactForm();
+    this.user= this.store.select('user');
+    console.log(this.user);
+    
   }
+  createContactForm() {
+    const params = {
+      title: ["", Validators.required],
+      name: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", Validators.compose([Validators.required, Validators.maxLength(250), Validators.pattern(this.utilsService.emailPattern)])],
+      phone: ["", [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(this.utilsService.onlyNumberPattern)]],
+      country: [""],
+      description:[""]
+    }
 
+    this.contactForm = this.formBuilder.group(params);
+  }
+  submitForm(){
+    if (this.contactForm.valid) {
+      let value = this.contactForm.value;
+      let  params = {
+        title: value.title,
+        name: value.name,
+        email: value.email,
+        phone: value.phone,
+        country: value.country,
+        description: value.description
+      }
+      console.log(params);
+    }
+  }
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class UtilsService {
 
   userInfo:any;
+  onlyNumberPattern = "^[0-9]*$";
+  emailPattern = "[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,63}";
   constructor() { }
 
 
